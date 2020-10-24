a9092f2ad7babe5f61a8859821769f1b19b0653a
joe <joe.chimienti@eventdynamic.com>
2020-10-24 10:27:16 -0400

    update playbooks

--- src/main/scala/com/mathbot/pay/lightning/ListPay.scala
+++ src/main/scala/com/mathbot/pay/lightning/ListPay.scala
@@ -7,5 +7,6 @@ case class ListPay(bolt11: Bolt11, status: PayStatus, amount_sent_msat: String)
 
 object ListPay {
   lazy implicit val formatListPay: OFormat[ListPay] = Json.format[ListPay]
-  def apply(bolt11: Bolt11, payment: Payment): ListPay = ListPay(bolt11, payment.status, payment.amount_sent_msat)
+  def apply(bolt11: Bolt11, payment: Payment): ListPay =
+    ListPay(bolt11 = bolt11, status = payment.status, amount_sent_msat = payment.amount_sent_msat)
 }
