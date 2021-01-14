150f5783d8106c89b19b53b93c3a7f4f88bd8d6c
Scala Steward <me@scala-steward.org>
2021-01-14 14:27:28 +0100

    Update model:core to 1.2.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   lazy val alpakkaSocketV = "2.0.2"
   lazy val macwireVersion = "2.3.7"
   lazy val nameofV = "1.0.3"
-  lazy val sttpModelV = "1.1.4"
+  lazy val sttpModelV = "1.2.0"
   lazy val bitcoinjV = "0.15.9"
   lazy val mockitoV = "3.2.3.0"
 
