8c3c4fce77b383dca49843d5657e2f398284b67c
Scala Steward <me@scala-steward.org>
2021-02-06 04:31:34 +0100

    Update scala-nameof to 3.0.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   lazy val sttpVersion = "2.2.9"
   lazy val alpakkaSocketV = "2.0.2"
   lazy val macwireVersion = "2.3.7"
-  lazy val nameofV = "1.0.3"
+  lazy val nameofV = "3.0.0"
   lazy val sttpModelV = "1.2.0"
   lazy val bitcoinjV = "0.15.9"
   lazy val mockitoV = "3.2.3.0"
