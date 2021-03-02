91707439fc0b8f373ab2544fd6aa29ca971c26b2
j-chimienti <jchimien@gmail.com>
2021-03-02 06:24:01 -0500

    add json support

--- src/main/scala/com/mathbot/pay/btcpayserver/InvoiceException.scala
+++ src/main/scala/com/mathbot/pay/btcpayserver/InvoiceException.scala
@@ -1,6 +1,9 @@
 package com.mathbot.pay.btcpayserver
 
+import play.api.libs.json.Json
+
 object InvoiceException extends Enumeration {
   type InvoiceException = Value
   val paidPartial, paidOver, paidLate, marked = Value
+  implicit val formatIE = Json.formatEnum(this)
 }
