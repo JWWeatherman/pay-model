2a66ee0ab83c51d47e3e20971bb1ee89e1311cb4
j-chimienti <jchimien@gmail.com>
2021-03-26 07:18:11 -0400

    format

--- src/test/scala/com/mathbot/pay/btcpayserver/ChargeInfoResponseTest.scala
+++ src/test/scala/com/mathbot/pay/btcpayserver/ChargeInfoResponseTest.scala
@@ -4,7 +4,6 @@ import org.scalatest
 import org.scalatest.matchers.should.Matchers
 import play.api.libs.json.{JsResultException, Json}
 
-
 object ChargeInfoResponseTest {
 
   val newCharge: String =
