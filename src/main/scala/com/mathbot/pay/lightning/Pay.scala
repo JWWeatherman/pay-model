011fde80f07167e942325a969b581a26ccab6c6f
j-chimienti <jchimien@gmail.com>
2021-04-24 12:27:29 -0400

    add Pay

---
+++ src/main/scala/com/mathbot/pay/lightning/Pay.scala
@@ -0,0 +1,24 @@
+package com.mathbot.pay.lightning
+
+import com.mathbot.pay.bitcoin.MilliSatoshi
+
+/**
+ * https://lightning.readthedocs.io/lightning-pay.7.html
+ * @param bolt11
+ * @param msatoshi
+ * @param label
+ * @param riskfactor
+ * @param maxfeepercent
+ * @param retry_for
+ * @param maxdelay
+ * @param exemptfee
+ */
+case class Pay(bolt11: Bolt11,
+               msatoshi: Option[MilliSatoshi],
+               label: Option[String],
+               riskfactor: Option[String],
+               maxfeepercent: Option[String],
+               retry_for: Option[String],
+               maxdelay: Option[String],
+               exemptfee: Option[String],
+) extends LightningJson
