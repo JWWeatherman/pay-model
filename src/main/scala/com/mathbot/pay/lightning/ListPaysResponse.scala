d7ec19e843286507625cc26563da3b70e30bab1b
j-chimienti <jchimien@gmail.com>
2021-06-21 19:09:25 -0400

    remove redundant extend

--- src/main/scala/com/mathbot/pay/lightning/ListPaysResponse.scala
+++ src/main/scala/com/mathbot/pay/lightning/ListPaysResponse.scala
@@ -2,7 +2,7 @@ package com.mathbot.pay.lightning
 
 import play.api.libs.json.{Json, OFormat}
 
-case class ListPaysResponse(jsonrpc: String, id: Long, result: Pays) extends Response[Pays] with LightningJson
+case class ListPaysResponse(jsonrpc: String, id: Long, result: Pays) extends Response[Pays]
 
 object ListPaysResponse {
   lazy implicit val formatListPaysResponse: OFormat[ListPaysResponse] = Json.format[ListPaysResponse]
