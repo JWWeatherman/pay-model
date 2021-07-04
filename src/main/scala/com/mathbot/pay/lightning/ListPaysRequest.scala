87a12e8d74f8eb85ab6dbe2cf4704d1b4207ebff
j-chimienti <jchimien@gmail.com>
2021-07-04 08:38:16 -0400

    add applys

--- src/main/scala/com/mathbot/pay/lightning/ListPaysRequest.scala
+++ src/main/scala/com/mathbot/pay/lightning/ListPaysRequest.scala
@@ -6,4 +6,8 @@ case class ListPaysRequest(bolt11: Option[Bolt11], payment_hash: Option[String])
 
 object ListPaysRequest {
   implicit val formatListPaysRequest: OFormat[ListPaysRequest] = Json.format[ListPaysRequest]
+
+  def apply(): ListPaysRequest = ListPaysRequest(None, None)
+  def apply(bolt11: Bolt11): ListPaysRequest = ListPaysRequest(Some(bolt11), None)
+  def apply(payment_hash: String): ListPaysRequest = ListPaysRequest(None, Some(payment_hash))
 }
