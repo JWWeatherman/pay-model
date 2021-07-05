2e353387a372c7d68302a7c1c0cdf690d70ee523
j-chimienti <jchimien@gmail.com>
2021-07-05 09:27:41 -0400

    add formatter

--- src/main/scala/com/mathbot/pay/lightning/DecodePayRequest.scala
+++ src/main/scala/com/mathbot/pay/lightning/DecodePayRequest.scala
@@ -1,3 +1,9 @@
 package com.mathbot.pay.lightning
 
+import play.api.libs.json.Json
+
 case class DecodePayRequest(bolt11: Bolt11) extends LightningJson
+
+object DecodePayRequest {
+  implicit val formatDecodePayRequest = Json.format[DecodePayRequest]
+}
