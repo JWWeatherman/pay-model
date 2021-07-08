e9ef76cf2a96792c6446b96613d2227453ff0eac
j-chimienti <jchimien@gmail.com>
2021-07-08 08:19:14 -0400

    change type to MilliSatoshi from string

--- src/main/scala/com/mathbot/pay/lightning/LightningOfferRequest.scala
+++ src/main/scala/com/mathbot/pay/lightning/LightningOfferRequest.scala
@@ -1,5 +1,6 @@
 package com.mathbot.pay.lightning
 
+import com.mathbot.pay.bitcoin.MilliSatoshi
 import play.api.libs.json.Json
 
 /**Create an offer for invoices of {amount} with {description}, optional {vendor}, internal {label}, {quantity_min}, {quantity_max}, {absolute_expiry}, {recurrence}, {recurrence_base}, {recurrence_paywindow}, {recurrence_limit} and {single_use}
@@ -9,8 +10,8 @@ case class LightningOfferRequest(amount: String,
                                  description: String,
                                  label: Option[String],
                                  vendor: Option[String],
-                                 quantity_min: Option[String],
-                                 quantity_max: Option[String],
+                                 quantity_min: Option[MilliSatoshi],
+                                 quantity_max: Option[MilliSatoshi],
                                  single_use: Option[Boolean])
     extends LightningJson
 
