f77ef5a753b5b39c9bfb554bb6b8ca6df20a905b
j-chimienti <jchimien@gmail.com>
2021-07-15 10:46:49 -0400

    cast to MilliSatoshi for received in ListInvoice

--- src/main/scala/com/mathbot/pay/lightning/ListInvoice.scala
+++ src/main/scala/com/mathbot/pay/lightning/ListInvoice.scala
@@ -15,7 +15,7 @@ case class ListInvoice(
     amount_msat: Option[String],
     status: LightningInvoiceStatus,
     pay_index: Option[Long],
-    msatoshi_received: Option[Long],
+    msatoshi_received: Option[MilliSatoshi],
     amount_received_msat: Option[String],
     paid_at: Option[Instant],
     description: String,
