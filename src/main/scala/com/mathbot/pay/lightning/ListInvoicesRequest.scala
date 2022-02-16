28916029ebfdf27b56695094047a0b951c968c1f
j-chimienti <jchimien@gmail.com>
2022-02-16 07:55:38 -0500

    add offer_id as param

--- src/main/scala/com/mathbot/pay/lightning/ListInvoicesRequest.scala
+++ src/main/scala/com/mathbot/pay/lightning/ListInvoicesRequest.scala
@@ -5,8 +5,12 @@ import play.api.libs.json.Json
 case class ListInvoicesRequest(
     label: Option[String] = None,
     invstring: Option[String] = None,
-    payment_hash: Option[String] = None
-) extends LightningJson
+    payment_hash: Option[String] = None,
+    offer_id: Option[String] = None
+) extends LightningJson {
+  val nonEmptyParams = List(label, invstring, payment_hash, offer_id).flatten
+  require(nonEmptyParams.length <= 1, "Invalid request")
+}
 
 object ListInvoicesRequest {
   implicit val formatListInvoicesRequest = Json.format[ListInvoicesRequest]
